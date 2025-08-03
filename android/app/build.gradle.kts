1fa6ea3f64f9b1413b737293a250102363246b6c
Mohammed Dawood <mohidawod@gmail.com>
2025-08-03 04:49:49 +0400

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 }
 
 android {
-    namespace = "com.example.radiokapp"
+    namespace = "com.dawood.radiokapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
