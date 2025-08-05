4a13aacf2cbcce9fef5753ee90711926c03388a6
Mohammed Dawood <mohidawod@gmail.com>
2025-08-05 15:33:47 +0400

    Enable background audio playback

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import AVFoundation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,13 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    do {
+      let session = AVAudioSession.sharedInstance()
+      try session.setCategory(.playback)
+      try session.setActive(true)
+    } catch {
+      print("Failed to set audio session category: \(error)")
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
